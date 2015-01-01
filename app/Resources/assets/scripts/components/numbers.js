a27fe99f175b1a043ff09c70da061a9fbcde6845
neemzy <tom.panier@free.fr>
2015-01-01 19:58:50 +0100

    simplified react class usage

--- app/Resources/assets/scripts/components/numbers.js
+++ app/Resources/assets/scripts/components/numbers.js
@@ -46,18 +46,16 @@
                     this.handleInput('remaining', value);
                 }.bind(this),
 
-                cx = React.addons.classSet,
-
-                estimateClass = cx({
+                estimateClass = React.addons.classSet({
                     'numbers__estimate': true
                 }),
 
-                consumedClass = cx({
+                consumedClass = React.addons.classSet({
                     'numbers__consumed': true,
                     'numbers__consumed--over': this.props.overConsumed
                 }),
 
-                remainingClass = cx({
+                remainingClass = React.addons.classSet({
                     'numbers__remaining': true,
                     'numbers__remaining--over': this.props.underEstimated && 0 < this.props.remaining,
                     'numbers__remaining--under': this.props.overEstimated
