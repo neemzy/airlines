ec49d0297d774893d0830b137649634357b26deb
neemzy <tom.panier@free.fr>
2015-01-13 23:21:43 +0100

    added task merging unit test

--- src/Airlines/AppBundle/Tests/Manager/TaskManagerTest.php
+++ src/Airlines/AppBundle/Tests/Manager/TaskManagerTest.php
@@ -143,4 +143,81 @@ class TaskManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($consumed / 2, $task->getConsumed());
         $this->assertEquals($remaining / 2, $task->getRemaining());
     }
+
+
+
+    /**
+     * Checks task merging
+     *
+     * @return void
+     *
+     * @testdox Can merge two tasks
+     */
+    public function testMerge()
+    {
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+                   ->disableOriginalConstructor()
+                   ->setMethods(['persist', 'remove', 'flush'])
+                   ->getMock();
+
+        $em->expects($this->once())
+           ->method('persist');
+
+        $em->expects($this->once())
+           ->method('remove');
+
+        $em->expects($this->once())
+           ->method('flush');
+
+        $validator = $this->getMockBuilder('Symfony\Component\Validator\Validator')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $router = $this->getMockBuilder('Symfony\Component\Routing\Router')
+                       ->disableOriginalConstructor()
+                       ->getMock();
+
+        $manager = new TaskManager($em, $validator, $router);
+
+        $name = 'Yet another task name';
+        $estimate1 = 2;
+        $consumed1 = 1;
+        $remaining1 = 1.5;
+        $estimate2 = 3;
+        $consumed2 = 2;
+        $remaining2 = 0.5;
+
+        $task1 = $this->getMockBuilder('Airlines\AppBundle\Entity\Task')
+                      ->setMethods(['getId', 'getName', 'getEstimate', 'getConsumed', 'getRemaining'])
+                      ->getMock();
+
+        $task1->expects($this->once())
+              ->method('getId')
+              ->will($this->returnValue(1));
+
+        $task1->expects($this->once())
+              ->method('getEstimate')
+              ->will($this->returnValue($estimate1));
+
+        $task1->expects($this->once())
+              ->method('getConsumed')
+              ->will($this->returnValue($consumed1));
+
+        $task1->expects($this->once())
+              ->method('getRemaining')
+              ->will($this->returnValue($remaining1));
+
+        $task2 = new Task();
+        $task2->setName($name);
+        $task2->setEstimate($estimate2);
+        $task2->setConsumed($consumed2);
+        $task2->setRemaining($remaining2);
+
+        $task2 = $manager->merge($task1, $task2);
+
+        $this->assertEquals($name, $task2->getName());
+        $this->assertEquals($estimate1 + $estimate2, $task2->getEstimate());
+        $this->assertEquals($consumed1 + $consumed2, $task2->getConsumed());
+        $this->assertEquals($remaining1 + $remaining2, $task2->getRemaining());
+    }
 }
