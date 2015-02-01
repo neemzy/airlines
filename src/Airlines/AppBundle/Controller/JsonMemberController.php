3f9ff2c32dc606fb31c5436fa0e1f291cb7cca8f
neemzy <tom.panier@free.fr>
2015-02-01 17:52:09 +0100

    added serialization event listeners to remove ugly fixes and simplified chained method calls syntax

--- src/Airlines/AppBundle/Controller/JsonMemberController.php
+++ src/Airlines/AppBundle/Controller/JsonMemberController.php
@@ -26,23 +26,12 @@ class JsonMemberController extends AbstractJsonController
      */
     public function listAction(Board $board)
     {
-        $em = $this->getDoctrine()->getManager();
-        $members = $em->getRepository('AirlinesAppBundle:Member')->findByBoard($board);
+        $members = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('AirlinesAppBundle:Member')
+            ->findByBoard($board);
 
-        $response = $this->createJsonResponse($members);
-
-        // FIXME : this ugly solution is here because I have *no* fucking idea
-        // how to add a custom property to the JSON sput out by JMS's Serializer.
-        // Defining a handler allows you to send whatever the fuck you want *instead*,
-        // but *no* hints about how to keep everything working like default
-        // and just add a goddamn fucking field to it. You're welcome.
-        $manager = $this->get('airlines.member_manager');
-        $content = json_decode($response->getContent());
-        foreach ($content as &$member) {
-            $member->taskUrl = $manager->generateRootTaskUrl($em->getRepository('AirlinesAppBundle:Member')->find($member->id));
-        }
-        $response->setContent(json_encode($content));
-
-        return $response;
+        return $this->createJsonResponse($members);
     }
 }
