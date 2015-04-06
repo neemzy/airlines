2c30b6fe1608222fb2617f961fd8a9fc8f646e6f
neemzy <tom.panier@free.fr>
2015-04-06 18:17:35 +0200

    refacto

--- app/Resources/assets/scripts/components/numbers.js
+++ app/Resources/assets/scripts/components/numbers.js
@@ -38,14 +38,9 @@
             var estimate = this.props.estimate.toFixed(3),
                 consumed = this.props.consumed.toFixed(3),
                 remaining = this.props.remaining.toFixed(3),
-                isEditable = undefined !== this.props.handleInput,
-                isOverConsumed = parseFloat(consumed) > parseFloat(estimate),
-                wasUnderEstimated = parseFloat(consumed) + parseFloat(remaining) > parseFloat(estimate),
-                wasOverEstimated = parseFloat(consumed) + parseFloat(remaining) < parseFloat(estimate),
-                handleEstimateInput,
-                handleConsumedInput,
-                handleRemainingInput,
-                content,
+                estimateNode = (<span className="numbers__value">{estimate}</span>),
+                consumedNode = (<span className="numbers__value">{consumed}</span>),
+                remainingNode = (<span className="numbers__value">{remaining}</span>),
 
                 estimateClass = classNames({
                     'numbers__estimate': true
@@ -53,59 +48,47 @@
 
                 consumedClass = classNames({
                     'numbers__consumed': true,
-                    'is-over': isOverConsumed
+                    'is-over': parseFloat(consumed) > parseFloat(estimate)
                 }),
 
                 remainingClass = classNames({
                     'numbers__remaining': true,
-                    'is-over': wasUnderEstimated && 0 < this.props.remaining,
-                    'is-under': wasOverEstimated
+                    'is-over': (parseFloat(consumed) + parseFloat(remaining) > parseFloat(estimate)) && (0 < this.props.remaining),
+                    'is-under': parseFloat(consumed) + parseFloat(remaining) < parseFloat(estimate)
                 });
 
-            if (isEditable) {
-                handleEstimateInput = function(value) {
-                    this.handleInput('estimate', value);
-                }.bind(this);
+            // Editable values ?
+            if (undefined !== this.props.handleInput) {
+                var handleEstimateInput = function(value) {
+                        this.handleInput('estimate', value);
+                    }.bind(this),
 
-                handleConsumedInput = function(value) {
-                    this.handleInput('consumed', value);
-                }.bind(this);
+                    handleConsumedInput = function(value) {
+                        this.handleInput('consumed', value);
+                    }.bind(this),
 
-                handleRemainingInput = function(value) {
-                    this.handleInput('remaining', value);
-                }.bind(this);
+                    handleRemainingInput = function(value) {
+                        this.handleInput('remaining', value);
+                    }.bind(this);
 
-                return (
-                    <div className="numbers">
-                        <div className={estimateClass}>
-                            <span className="numbers__label">Estimate</span>
-                            <Editable title="Estimate" className="numbers__value" handleInput={handleEstimateInput}>{estimate}</Editable>
-                        </div>
-                        <div className={consumedClass}>
-                            <span className="numbers__label">Consumed</span>
-                            <Editable title="Consumed" className="numbers__value" handleInput={handleConsumedInput}>{consumed}</Editable>
-                        </div>
-                        <div className={remainingClass}>
-                            <span className="numbers__label">Remaining</span>
-                            <Editable title="Remaining" className="numbers__value" handleInput={handleRemainingInput}>{remaining}</Editable>
-                        </div>
-                    </div>
-                );
+                estimateNode = (<Editable className="numbers__value" handleInput={handleEstimateInput}>{estimate}</Editable>);
+                consumedNode = (<Editable className="numbers__value" handleInput={handleConsumedInput}>{consumed}</Editable>);
+                remainingNode = (<Editable className="numbers__value" handleInput={handleRemainingInput}>{remaining}</Editable>);
             }
 
             return (
                 <div className="numbers">
                     <div className={estimateClass} title="Estimate">
                         <span className="numbers__label">Estimate</span>
-                        <span className="numbers__value">{estimate}</span>
+                        {estimateNode}
                     </div>
                     <div className={consumedClass} title="Consumed">
                         <span className="numbers__label">Consumed</span>
-                        <span className="numbers__value">{consumed}</span>
+                        {consumedNode}
                     </div>
                     <div className={remainingClass} title="Remaining">
                         <span className="numbers__label">Remaining</span>
-                        <span className="numbers__value">{remaining}</span>
+                        {remainingNode}
                     </div>
                 </div>
             );
